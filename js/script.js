2211c2f24dbf4dcce20cb8994250b98d96a7443b
Pratap Aditya <prataparya2012@gmail.com>
2025-02-07 18:01:17 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -289,3 +289,16 @@ $('#status').fadeOut(); // will first fade out the loading animation
 $('#preloader').delay(50).fadeOut(100); // will fade out the white DIV that covers the website.
 $('body').delay(50).css({'overflow':'visible'});
 })
+
+
+$(document).ready(function() {
+  setGalleryTransition("slideAndZoom");
+            enableExtraButtons();
+           
+            json = '{ "Entry": { "Title": "", '+
+               '"Image": ["",'+       
+               '"" ] } }';
+           addGallery(json);
+           json = '{ "Entry": { "Title": "",  "Image": ["", "" ] } }';
+           addGallery(json);
+		   });
